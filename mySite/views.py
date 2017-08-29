51617d3741f02fcb0dd443f05ba5ce7e19ee6861
AlexanderMcNulty <alexander.mcnulty92@gmail.com>
2017-08-29 11:56:27 -0700

    fix development.ini, fix views, video 12/48

--- mySite/views.py
+++ mySite/views.py
@@ -1,3 +1,4 @@
+from pyramid.httpexceptions import HTTPFound
 from pyramid.view import view_config
 
 
@@ -15,7 +16,7 @@ def add(request):
 
 @view_config(route_name='view',
              renderer='templates/view.jinja2')
-def add(request):
+def view(request):
     # the id we are looking for has been encoded in the route parameter
     # request.matchdict provides a dictionary for all of the matching route parameters
     # .get is used to avoid a python key error in case we ask for a parameter that is not in the dictionary
@@ -29,6 +30,8 @@ def edit(request):
     current = request.matchdict.get('id')
     return dict(current=current)
 
+
 @view_config(route_name='delete')
-def add(request):
-    return dict()
+def delete(request):
+    url = request.route_url('list')
+    return HTTPFound(url)
--- setup.py
+++ setup.py
@@ -6,9 +6,6 @@ requires = [
 ]
 setup(name='mysite',
       install_requires=requires,
-      # place of plug in points for other packages in this package
-      # currently being used to defines location to be used to get a WSGI application (web services gateway interface)
-      # in our case it is the the main function in the mysite package
       entry_points="""\
       [paste.app_factory]
       main = mysite:main
