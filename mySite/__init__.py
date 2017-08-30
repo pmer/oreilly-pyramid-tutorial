4664abd4206ea874c71cf212a78abb1b6462a1ab
AlexanderMcNulty <alexander.mcnulty92@gmail.com>
2017-08-30 00:24:31 -0700

    14/48, view parameters

--- mySite/__init__.py
+++ mySite/__init__.py
@@ -21,8 +21,6 @@ def main(global_config, **settings):
     config.include('pyramid_jinja2')
     config.add_route('list', '/')
     config.add_route('add', '/add')
-    # these are route parameters; the '{id}'
-    # matches urls to data
     config.add_route('view', '/{id}')
     config.add_route('edit', '/{id}/edit')
     config.add_route('delete', '/{id}/delete')
--- mySite/views.py
+++ mySite/views.py
@@ -5,6 +5,8 @@ from pyramid.view import view_config
 # but differ in another way for example by their request_method ie. GET, POST, DELETE, PUT
 
 # classes are ideal for sharing state, computation, or other kinds of coupling
+
+# when transitioning from view functions to a function class all the parameter we passed called request should change to self
 class MySite:
     def __init__(self, request):
         self.request = request
@@ -16,25 +18,37 @@ class MySite:
 
     @view_config(route_name='list',
                  renderer='templates/list.jinja2')
-    def list(request):
+    def list(self):
         return dict()
 
     @view_config(route_name='add',
                  renderer='templates/add.jinja2')
-    def add(request):
+    def add(self):
         return dict()
 
     @view_config(route_name='view',
                  renderer='templates/view.jinja2')
-    def view(request):
+    def view(self):
         return dict()
 
     @view_config(route_name='edit',
                  renderer='templates/edit.jinja2')
-    def edit(request):
+    def edit(self):
         return dict()
 
+    # the parameters passed to @view_config are all examples of predicates
+    # what is an example of a custom predicate?
+    @view_config(route_name='edit',
+                 renderer='templates/edit.jinja2',
+                 request_method='POST',
+                 request_param='form.submit')
+    def edit_handler(self):
+        new_title = self.request.params.get('new_title')
+        print('New title', new_title)
+        url = self.request.route_url('list')
+        return HTTPFound(url)
+
     @view_config(route_name='delete')
-    def delete(request):
-        url = request.route_url('list')
+    def delete(self):
+        url = self.route_url('list')
         return HTTPFound(url)
